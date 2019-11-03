1964e227ca0cb437d44ce1e5c51a385f7ba32102
jonatansalas <jonatansalas@live.com.ar>
2019-11-03 14:39:58 -0300

    fix(fields): missing DependentField export from lib

--- packages/rff-fields/src/index.ts
+++ packages/rff-fields/src/index.ts
@@ -1,3 +1,4 @@
 export * from './components/PrefixedField';
+export * from './components/DependentField';
 export * from './components/ConditionalField';
 export * from './components/WhenFieldChanges';
