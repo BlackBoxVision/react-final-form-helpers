1ae844d98baa32ba190e8f4a1b9b9f14e4b4e201
Manuel Tuero <manuel.tuero@blackbox-vision.tech>
2021-01-31 19:34:19 -0300

    feat: add textinput component. modify readme file

--- packages/rff-material-ui/src/components/TextInput/index.tsx
+++ packages/rff-material-ui/src/components/TextInput/index.tsx
@@ -1,8 +1,33 @@
-import React  from 'react';
-import { Field } from 'react-final-form';
+import React, { FC } from 'react';
+import { FieldRenderProps } from 'react-final-form';
+import { TextField } from '@material-ui/core';
 
-export const TextInput = () => {
-  return <div />;
+import { useHasError } from '../../hooks/useHasError';
+
+export type TextInputProps = FieldRenderProps<string, any>;
+
+export const TextInput: FC<TextInputProps> = ({
+  name,
+  label,
+  variant,
+  input,
+  meta,
+  ...rest
+}) => {
+  const isError = useHasError(meta);
+
+  return (
+    <TextField
+      {...rest}
+      error={isError}
+      label={label}
+      variant={variant}
+      name={input.name}
+      value={input.value}
+      onChange={input.onChange}
+      helperText={isError && meta.error}
+    />
+  );
 };
 
 TextInput.displayName = 'TextInput';
--- packages/rff-material-ui/stories/TextInput.stories.tsx
+++ packages/rff-material-ui/stories/TextInput.stories.tsx
@@ -1,19 +1,50 @@
 import { ComponentProps } from 'react';
+import { Form, Field } from 'react-final-form';
 import { Story } from '@storybook/react';
 
-import { TextInput } from '../src/index';
+import { TextInput } from '../src';
 
 // This default export determines where your story goes in the story list
 export default {
   title: 'TextInput',
   component: TextInput,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'radio',
+        options: ['standard', 'filled', 'outlined'],
+      },
+    },
+  },
 };
 
+const onSubmit = async (values) =>
+  window.alert(JSON.stringify(values, null, 2));
+
 const Template: Story<ComponentProps<typeof TextInput>> = (args) => (
-  <TextInput {...args} />
+  <Form
+    onSubmit={onSubmit}
+    render={({ handleSubmit }) => (
+      <form onSubmit={handleSubmit} style={{ display: 'flex' }}>
+        <Field
+          {...args}
+          name={args.name}
+          variant={args.variant}
+          component={TextInput}
+          validate={(value) => (value ? undefined : 'Required field')}
+        />
+        <button type="submit" style={{ marginLeft: 16 }}>
+          Send
+        </button>
+      </form>
+    )}
+  />
 );
 
-export const FirstStory = Template.bind({});
-FirstStory.args = {
-  /* the args you need here will depend on your component */
+export const SimpleExample = Template.bind({});
+
+SimpleExample.args = {
+  name: 'email',
+  label: 'Email',
+  variant: 'standard',
 };
