cc3b904e7a6086ed1fb706803a3c87a6a335c739
JonatanSalas <jonatan.salas@blackbox-vision.tech>
2022-04-04 14:52:29 -0300

    feat(rff-rn-paper): export Container and useHasError to enable the posibility of building custom inputs

--- packages/rff-rn-paper/src/index.tsx
+++ packages/rff-rn-paper/src/index.tsx
@@ -1,6 +1,9 @@
 export * from './components/Switch';
 export * from './components/Select';
 export * from './components/Checkbox';
+export * from "./components/Container";
 export * from './components/TextInput';
 export * from './components/RadioButton';
 export * from './components/RadioButtonGroup';
+
+export * from './hooks/useHasError';
